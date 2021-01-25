05a5a07463bb7e27dab252ed88ac1680f91662d8
Robert Boyd III <rob@robertcboyd.com>
2021-01-25 14:58:44 -0500

    Remove unnecessary hello

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ const data = document.querySelector("#data");
 const button = document.querySelector("#button");
 const searchbox = document.querySelector("#search");
 
-data.innerHTML = "Hello from JavaScript!";
-
 button.addEventListener("click", onClick);
 
 function onClick() {
