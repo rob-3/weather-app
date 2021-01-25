296dabb308b2fd6bb1fc8047a097a6c4838ffcb8
Robert Boyd III <rob@robertcboyd.com>
2021-01-25 14:58:27 -0500

    Add searchbox

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const data = document.querySelector("#data");
 const button = document.querySelector("#button");
+const searchbox = document.querySelector("#search");
 
 data.innerHTML = "Hello from JavaScript!";
 
 button.addEventListener("click", onClick);
 
 function onClick() {
-  console.log("Clicked!");
+  const input = searchbox.value;
+  console.log("You typed: " + input);
 }
