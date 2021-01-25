cfa76701b8bf765fad22e9b04f4d80a6b35b36f9
Robert Boyd III <rob@robertcboyd.com>
2021-01-25 14:55:28 -0500

    Add a simple button

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const data = document.querySelector("#data");
+const button = document.querySelector("#button");
 
 data.innerHTML = "Hello from JavaScript!";
+
+button.addEventListener("click", onClick);
+
+function onClick() {
+  console.log("Clicked!");
+}
